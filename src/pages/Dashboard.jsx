3d7f752238dabe97145955006cd7251dba6b0ffc
Francis Martinez <f.martinez1993@icloud.com>
2026-01-17 22:26:29 -0500

    Test Color for overscroll in safari and ios

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Dashboard() {
   return (
-    <div className="w-full h-dvh bg-amber-300">
+    <div className="w-full h-dvh">
       <h1 className="text-7xl">Dashboard</h1>
     </div>
   );

e757e4eef8128032047d9b1aeb9c6cb4fe1cea57
Francis Martinez <f.martinez1993@icloud.com>
2026-01-19 19:19:52 -0500

    logo perfect display

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import { useContext } from "react";
 import { SidebarContext } from "../context/SidebarContext";
 import SidebarButton from "./SidebarButton";
+import { logo } from "../imports/images";
 
 function Sidebar() {
   const { expanded } = useContext(SidebarContext);
 
   return (
     <div
-      className={`bg-(--second-color) pl-2 py-2 h-full transition-all duration-300 flex items-center ${expanded ? "w-50" : "w-15"}`}
+      className={`bg-(--second-color) py-2 h-full transition-all duration-300 flex flex-col items-center justify-between ${expanded ? "w-50" : "w-20"}`}
     >
-      <SidebarButton btnName={"Dashboard"} />
+      <div className="w-full h-12 flex items-center justify-center ">
+        <img
+          src={expanded ? logo.dark : logo.dark_square}
+          className="h-12 transition-all "
+        />
+      </div>
+      <div className="w-5/6 h-12 flex items-center justify-center ">
+        <SidebarButton btnName={"Dashboard"} />
+      </div>
+      <div className="w-full h-12 flex items-center justify-center ">
+        {/**/}
+      </div>
     </div>
   );
 }
---
+++ src/imports/images.js
@@ -0,0 +1,14 @@
+//Logo
+import Logo_Dark from '../assets/logo/JSMNworks_logo_Dark.png'
+import Logo_Light from '../assets/logo/JSMNworks_logo_Light.png'
+import Logo_Dark_square from '../assets/logo/JSMNworks_logo_square_Dark.png'
+import Logo_Light_square from '../assets/logo/JSMNworks_logo_square_Light.png'
+
+const logo = {
+    dark: Logo_Dark,
+    light: Logo_Light,
+    dark_square: Logo_Dark_square,
+    light_square: Logo_Light_square,
+}
+
+export {logo}
